52781e87cbc22814e7fa3127ea15eabdbdae1a76
Alex Zylman <alex.zylman@clever.com>
2015-01-21 15:58:07 -0800

    sphinx: slide cleanup

--- interfaces-sphinx/http_limiter_examples/constructors.go
+++ interfaces-sphinx/http_limiter_examples/constructors.go
@@ -6,14 +6,10 @@ import (
 )
 
 // START OMIT
-
-// NewHTTPLimiter returns an http.Handler that rate limits and proxies requests.
 func NewHTTPLimiter(rateLimiter ratelimiter.RateLimiter, proxy http.Handler) http.Handler {
 	return &httpRateLimiter{rateLimiter: rateLimiter, proxy: proxy}
 }
 
-// NewHTTPLogger returns an http.Handler that logs the results of rate limiting requests, but
-// actually proxies everything.
 func NewHTTPLogger(rateLimiter ratelimiter.RateLimiter, proxy http.Handler) http.Handler {
 	return &httpRateLogger{rateLimiter: rateLimiter, proxy: proxy}
 }
